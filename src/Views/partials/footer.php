bee95c91247b58bf7c8b11e160f93d1f8661780f
mmccc34 <mmccc34@icloud.com>
2025-01-14 12:45:11 +0100

    style du footer

--- src/Views/partials/footer.php
+++ src/Views/partials/footer.php
@@ -1,3 +1,7 @@
 <footer>
-    <h5>Ceci est mon footer</h5>
+    <div class="footer text-center p-5" style="background-color: #4b3550; color: white;">
+        <p style="color: #9b7b97;">Merci d'utiliser notre Application de Gestion de Projets</p>
+        <p class="mt-4" style="color: #aca2b3a1;">© 2024 Application de Gestion de Projets - Tous droits réservés</p>
+    </div>
+
 </footer>
