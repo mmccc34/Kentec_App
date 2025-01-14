1f0dd2842586480c9b6fe51fd950a60315ac8045
mmccc34 <mmccc34@icloud.com>
2025-01-14 09:46:51 +0100

    liens accueil

--- src/Views/home/index.php
+++ src/Views/home/index.php
@@ -3,8 +3,8 @@
     <p class="lead" style="color: #9b7b97;">Optimisez vos projets et simplifiez vos processus avec notre outil intuitif.</p>
     <div class="button-group mt-4">
         <a href="/users/list" class="btn" style="background-color: #6e3f56; color: white; padding: 10px 20px; border-radius: 25px; margin-right: 10px;">Gestion des Utilisateurs</a>
-        <a href="/clients/list" class="btn" style="background-color: #6e3f56; color: white; padding: 10px 20px; border-radius: 25px; margin-right: 10px;">Gestion des clients</a>
-        <a href="/projects" class="btn" style="background-color: #4b3550; color: white; padding: 10px 20px; border-radius: 25px; margin-right: 10px;">Gestion des Projets</a>
+        <a href="/client/list" class="btn" style="background-color: #6e3f56; color: white; padding: 10px 20px; border-radius: 25px; margin-right: 10px;">Gestion des clients</a>
+        <a href="/project/list" class="btn" style="background-color: #4b3550; color: white; padding: 10px 20px; border-radius: 25px; margin-right: 10px;">Gestion des Projets</a>
 
     </div>
 </div>
