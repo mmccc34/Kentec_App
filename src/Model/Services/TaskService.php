df9400ed6dffe1753e24620b18c14fd5d11024f6
Lucas <lucas.rom24@gmail.com>
2025-01-14 10:52:00 +0100

    basculement sur de nouveaux repos

--- src/Model/Services/TaskService.php
+++ src/Model/Services/TaskService.php
@@ -7,38 +7,38 @@ use Sthom\Kernel\Utils\Repository;
 
 class TaskService
 {
-    private Repository $repo;
+//     private Repository $repo;
 
-    public function __construct()
-    {
-        $this->repo=new Repository(task::class);        
-    }
+//     public function __construct()
+//     {
+//         $this->repo=new Repository(task::class);        
+//     }
 
-    public function getFullTaskById(int $id)
-    {
-        $query = "SELECT 
-    t.id AS taskId, 
-    t.name AS taskName, 
-    t.description AS taskDescription, 
-    t.startDate AS taskStartDate, 
-    t.endDate AS taskEndDate, 
-    t.effort AS taskEffort, 
-    u.name AS devName, 
-    u.firstname AS devFirstname, 
-    u.email AS devEmail, 
-    p.id AS projectId, 
-    p.name AS projectName, 
-    p.description AS projectDescription,
-    p.idManager AS projectIdManager, 
-    p.idState AS projectIdState, 
-    p.idClient AS projectIdClient, 
-    s.name AS stateName, 
-    s.id AS stateId
-FROM task t
-JOIN users u ON u.id = t.idDev
-JOIN project p ON p.id = t.idProject
-JOIN state s ON s.id = t.idState
-        WHERE t.id=:id ";
-        return $this->repo->customQuery($query, [":id" => $id])[0];
-    }
+//     public function getFullTaskById(int $id)
+//     {
+//         $query = "SELECT 
+//     t.id AS taskId, 
+//     t.name AS taskName, 
+//     t.description AS taskDescription, 
+//     t.startDate AS taskStartDate, 
+//     t.endDate AS taskEndDate, 
+//     t.effort AS taskEffort, 
+//     u.name AS devName, 
+//     u.firstname AS devFirstname, 
+//     u.email AS devEmail, 
+//     p.id AS projectId, 
+//     p.name AS projectName, 
+//     p.description AS projectDescription,
+//     p.idManager AS projectIdManager, 
+//     p.idState AS projectIdState, 
+//     p.idClient AS projectIdClient, 
+//     s.name AS stateName, 
+//     s.id AS stateId
+// FROM task t
+// JOIN users u ON u.id = t.idDev
+// JOIN project p ON p.id = t.idProject
+// JOIN state s ON s.id = t.idState
+//         WHERE t.id=:id ";
+//         return $this->repo->customQuery($query, [":id" => $id])[0];
+//     }
 }
